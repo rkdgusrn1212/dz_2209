945baf6b96fd3a53b4399b2a5f157019d4810be8
dreamtree123 <lovess_22@naver.com>
2022-09-08 20:34:31 +0900

    Quiz에 생성자 추가, QuizDAO에 쿼리문 추가

--- src/model/dao/QuizDAO.java
+++ src/model/dao/QuizDAO.java
@@ -5,22 +5,46 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import db.util.DBConnManager;
+import model.vo.Book;
 import model.vo.Quiz;
 
 public class QuizDAO {
     
-    public Quiz selectQuiz() {//전체 퀴즈 조회
+//    public Quiz selectQuiz() {//전체 퀴즈 조회 
+//        PreparedStatement pstmt = DBConnManager.getInstance().getPreparedStatement(
+//                "select * from quiz");
+//        ResultSet rs = null;
+//        try {
+//             rs = pstmt.executeQuery();
+//             while (rs.next()) {
+//                 Quiz q = new Quiz(
+//                                   rs.getInt("quizId"),
+//                                   rs.getInt("isbn"),
+//                                   rs.getString("question"),
+//                                   rs.getString("answer"));
+//             }
+//          } catch (SQLException e) {
+//             e.printStackTrace();
+//          } finally {
+//              DBConnManager.close(rs);
+//              DBConnManager.close(pstmt);
+//          }
+//        return null;
+//    }
+    
+    //퀴즈 문제 출력 (select question from quiz where quizId=1)
+    //(bookSelectView에서의 '도서 퀴즈' 버튼 클릭 시)
+    public Quiz selectQuizQuestion(int quizId) {
         PreparedStatement pstmt = DBConnManager.getInstance().getPreparedStatement(
-                "select * from quiz");
+                "select question from quiz where quizId = ?");
         ResultSet rs = null;
+        Quiz q = null;
+        
         try {
+//            pstmt.setInt(1, quizId);
              rs = pstmt.executeQuery();
              while (rs.next()) {
-                 Quiz q = new Quiz(
-                                   rs.getInt("quizId"),
-                                   rs.getInt("isbn"),
-                                   rs.getString("question"),
-                                   rs.getString("answer"));
+                 q = new Quiz( rs.getString("question"));
              }
           } catch (SQLException e) {
              e.printStackTrace();
@@ -28,8 +52,35 @@ public class QuizDAO {
               DBConnManager.close(rs);
               DBConnManager.close(pstmt);
           }
-        return null;
+        return q;
     }
+    
+    //퀴즈 정답 도출 (select question from quiz where quizId=1)
+    //뽑아낸 정답(=Answer)과 '라디오버튼 체크한 값'이 같은지 검사하기 위함
+    public Quiz selectQuizAnswer(int quizId) {
+        PreparedStatement pstmt = DBConnManager.getInstance().getPreparedStatement(
+                "select answer from quiz where quizId = ?");
+        ResultSet rs = null;
+        Quiz q = null;
+        
+        try {
+//            pstmt.setInt(1, quizId);
+             rs = pstmt.executeQuery();
+             while (rs.next()) {
+                 q = new Quiz( rs.getString("answer"));
+             }
+             
+//             return q;
+          } catch (SQLException e) {
+             e.printStackTrace();
+          } finally {
+              DBConnManager.close(rs);
+              DBConnManager.close(pstmt);
+          }
+        return q;
+    }
+    
+
     
     public boolean insertQuiz(Quiz q) {//AdminView에서의 퀴즈 추가  
         PreparedStatement pstmt = DBConnManager.getInstance().getPreparedStatement(
@@ -48,6 +99,26 @@ public class QuizDAO {
           }
         return false;
     }
+    
+    
+    
+//    public void insertQuiz(Quiz q) {//AdminView에서의 퀴즈 추가  
+//        PreparedStatement pstmt = DBConnManager.getInstance().getPreparedStatement(
+//                "insert into quiz values (?,?,?,?)");
+//         try {
+//             pstmt.setInt(1, q.getQuizId());
+//             pstmt.setInt(2, q.getIsbn());
+//             pstmt.setString(3, q.getQuestion());
+//             pstmt.setString(4, q.getAnswer());
+////             int t = pstmt.executeUpdate();
+////             if(t>0) return true;
+//          } catch (SQLException e) {
+//             e.printStackTrace();
+//          } finally {
+//              DBConnManager.close(pstmt);
+//          }
+////        return false;
+//    }
     
 //    public boolean insertQuiz(Quiz q) {//도서별 퀴즈 입력
 //        PreparedStatement pstmt = DBConnManager.getInstance().getPreparedStatement(
