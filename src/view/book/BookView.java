271f792ceb852f7be68034576cc743448d660106
taeeun7 <112858222+taeeun7@users.noreply.github.com>
2022-09-08 17:43:24 +0900

    bookview  되살리기

--- src/view/book/BookView.java
+++ src/view/book/BookView.java
@@ -1,109 +1,53 @@
 package view.book;
-
 import javax.swing.JButton;
-import javax.swing.JComboBox;
-import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JOptionPane;
-import javax.swing.JPasswordField;
+import javax.swing.JPanel;
 import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
-import javax.swing.JTextField;
 
-public class AddBookView extends JFrame 
-{
-     public JTextField tfIsbn, tfBook, tfWriter, tfCategory, tfPrice; 
-     public JTextArea taContent;
-     public JButton btnSubmit, btnReset;
-     JLabel labelIsbn,labelBook,labelWriter,labelCategory,labelPrice, labelContent;
-     JScrollPane sp;
-     
-     public AddBookView(){   
-         setTitle("AddBookView");
-      
-         tfIsbn = new JTextField();
-//          tf_book = new JPasswordField();
-//          tf_writer = new JPasswordField();
-         tfBook = new JTextField();
-         tfWriter = new JTextField();
-         tfCategory = new JTextField();
-         tfPrice = new JTextField();
-         taContent = new JTextArea();
-          
-         btnSubmit = new JButton("등록");
-         btnReset = new JButton("취소");
-          
-         labelIsbn = new JLabel("도서번호 : ");
-         labelBook = new JLabel("도서명 : ");
-         labelWriter = new JLabel("저자명 : ");
-         labelCategory = new JLabel("도서장르 : ");
-         labelPrice = new JLabel("도서원가 :"); 
-         labelContent = new JLabel("줄거리 :");
-          
-         
-         //setBounds 
-         tfIsbn.setBounds(80,30,100,25);
-         tfBook.setBounds(80,70,100,25);
-         tfWriter.setBounds(80,110,100,25);
-         tfCategory.setBounds(80,150,100,25);
-         tfPrice.setBounds(80,190,100,25);
-         sp = new JScrollPane(taContent);
-         sp.setBounds(80,230,180,60);
-          
-         btnSubmit.setBounds(50,310,90,25);
-         btnReset.setBounds(150,310,90,25);
-          
-         labelIsbn.setBounds(10,30,100,25);
-         labelBook.setBounds(10,70,100,25);
-         labelWriter.setBounds(10,110,100,25);
-         labelCategory.setBounds(10,150,100,25);
-         labelPrice.setBounds(10, 190, 100, 25);
-         labelContent.setBounds(10, 230, 100, 25);
-         
-         
-          //add
-          setLayout(null);
-          add(tfIsbn);
-          add(tfBook);
-          add(tfWriter);
-          add(tfCategory);
-          add(tfPrice);
-//          add(taContent);
-          
-          add(btnSubmit);
-          add(btnReset);
-          
-          add(labelIsbn); 
-          add(labelBook); 
-          add(labelWriter); 
-          add(labelCategory);
-          add(labelPrice);
-          add(labelContent);
-          add(sp);
-          
-          setBounds(350,200,300,400); 
-          
-          setVisible(true);
-         }
-     
-     
-          //생성자  
-          public void showMsg(String msg) {
-          JOptionPane.showMessageDialog(this, msg);
-         }
-          
-          public void setEmpty() {
-              tfIsbn.setText("");
-              tfBook.setText("");
-              tfWriter.setText("");
-              tfCategory.setText("");
-              tfPrice.setText("");
-          }
-          
-          
-          //main
-      public static void main(String[] args) {
-        new AddBookView();
+import view.View;
+
+public class BookView extends View{
+    JLabel labelBook, labelName, labelWriter, labelPrice, labelSummary;
+    public JButton btnBack;
+    JButton btnPay;
+    JPanel panelBook;
+    
+//    JTable table; //검색과 전체 보기를 위한 테이블 객체 생성
+    public BookView() {
+        getContentPane().setLayout(null);
+        labelBook = new JLabel("도서 상세 보기");
+        labelName = new JLabel("도서명");
+        labelWriter = new JLabel("저자명");
+        labelPrice = new JLabel("원가");
+        labelSummary = new JLabel("줄거리");
+        btnBack = new JButton("메인페이지");
+        btnPay = new JButton("결제");
+        panelBook = new JPanel();
+        
+        // setBounds
+        panelBook.setBounds(133, 54, 1164, 615);
+        btnBack.setBounds(20, 13, 105, 27);
+        btnPay.setBounds(150, 13, 105, 27);
+        labelBook.setBounds(450, 12, 105, 28);
+        labelName.setBounds(40, 100, 105, 28);
+        labelPrice.setBounds(40, 170, 105, 27);
+        labelWriter.setBounds(40, 240, 50, 28);
+        labelSummary.setBounds(40, 310, 50, 28);
+        
+        
+        //테이블 객체 생성
+        add(new JScrollPane(panelBook = new JPanel()), "Center");
+        getContentPane().add(labelBook);
+        getContentPane().add(btnBack);
+        getContentPane().add(panelBook);
+        add(btnPay);
+        add(labelName);
+        add(labelWriter);
+        add(labelPrice);
+        add(labelSummary);
+        
+        setBounds(500,200,1000,700);
+        
+        
     }
-      
-}//AddBookView
+} 
