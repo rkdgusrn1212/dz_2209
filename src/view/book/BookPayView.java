84d4d3609091516d1893c0aa36af43be4ac61f7c
3002291 <112863906+3002291@users.noreply.github.com>
2022-09-08 12:42:49 +0900

    BookPayView 전체 UI 변경

--- src/view/book/BookPayView.java
+++ src/view/book/BookPayView.java
@@ -1,99 +1,48 @@
 package view.book;
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-import javax.swing.JTextArea;
+import javax.swing.JTable;
 import javax.swing.JTextField;
 public class BookPayView extends JFrame {
-    public JPanel panelBook, panelPay;
-    public JLabel labelImage, labelBook, labelWriter, labelPrice;
-    public JTextArea taContent;
-    public JLabel labelId, labelGrade, labelPoint, labelLend, labelShowPoint;
+	public JTable table;
+    public JLabel labelId, labelCash, labelPoint, labelLend, labelUsePoint;
     public JTextField tfPoint;
     public JButton btnPay, btnBack;
     public BookPayView() {
         setTitle("BookPayView");
-        panelBook = new JPanel();
-        panelBook.setBackground(Color.CYAN);
-        panelPay = new JPanel();
-        panelPay.setBackground(Color.white);
-        panelBook.setLayout(null);
-        
-        //p_pay
-        panelPay.setLayout(null);
-        labelId = new JLabel("회원 id");
-        labelGrade = new JLabel("회원 등급");
-        labelPoint = new JLabel("회원 포인트");
-        labelLend = new JLabel("대여금액: 2000원");
-        labelShowPoint = new JLabel("사용 포인트: ");
+        setLayout(null);
+     
+        //p_book
+        labelId = new JLabel("회원 ID");
+        labelCash = new JLabel("회원 보유 캐시");
+        labelPoint = new JLabel("회원 보유 포인트");
+        labelUsePoint= new JLabel("사용 포인트");
+        labelLend = new JLabel("대여 가격");
         tfPoint = new JTextField(10);
         btnPay = new JButton("결제");
-        btnBack = new JButton("도서선택창");
-        labelId.setBounds(50, 50, 100, 30);
-        labelGrade.setBounds(50, 120, 100, 30);
-        labelPoint.setBounds(50, 190, 100, 30);
-        panelPay.add(labelId);
-        panelPay.add(labelGrade);
-        panelPay.add(labelPoint);
-        labelLend.setBounds(300, 50, 100, 30);
-        labelShowPoint.setBounds(300, 120, 100, 30);
-        tfPoint.setBounds(400, 120, 100, 30);
-        btnPay.setBounds(300, 200, 100, 30);
-        btnBack.setBounds(480, 200, 100, 30);
-        panelPay.add(labelLend);
-        panelPay.add(labelShowPoint);
-        panelPay.add(tfPoint);
-        panelPay.add(btnBack);
-        panelPay.add(btnPay);
-        //p_book
-        labelImage = new JLabel("도서 이미지");
-        labelBook = new JLabel("도서명");
-        taContent = new JTextArea();
-        labelPrice = new JLabel("도서 원가");
-        labelWriter = new JLabel("작가명");
-        labelImage.setBounds(50, 30, 200, 200);
-        labelBook.setBounds(300, 50, 100, 30);
-        labelWriter.setBounds(300, 100, 100, 30);
-        labelPrice.setBounds(300, 150, 100, 30);
-        taContent.setBounds(500, 30, 200, 200);
-        panelBook.add(labelImage);
-        panelBook.add(labelBook);
-        panelBook.add(labelPrice);
-        panelBook.add(labelWriter);
-        panelBook.add(taContent);
-        panelBook.setPreferredSize(new Dimension(0, 270));
-        add(panelBook, BorderLayout.PAGE_START);
-        add(panelPay, BorderLayout.CENTER);
-        setBounds(500,200,750,600);
-        setVisible(true);
+      	btnBack = new JButton("이전으로");
         
+        labelId.setBounds(50, 30, 50, 50);
+        labelCash.setBounds(50, 100, 80, 50);
+        labelPoint.setBounds(50, 170, 90, 50);
+        labelLend.setBounds(350, 30, 70, 50);
+        labelUsePoint.setBounds(350, 100, 90, 50);
+        tfPoint.setBounds(430, 110, 120, 30);
+        btnPay.setBounds(350, 190, 100, 30);
+        btnBack.setBounds(510, 190, 100, 30);
         
-        //버튼클릭시 화면 전환 
-        btnBack.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                new BookSelectView();
-                setVisible(false);              
-            }        
-        });
+        add(labelId);
+        add(labelCash);
+        add(labelPoint);
+        add(labelLend);
+        add(labelUsePoint);
+        add(tfPoint);
+        add(btnPay);
+        add(btnBack);
+
         
-        btnPay.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {               
-                JOptionPane.showMessageDialog(null, "결제되었습니다");
-            }
-        });
+        setBounds(500,200,700,300);
         
-        
-    }
-    public static void main(String[] args) {
-        new BookPayView().setVisible(true);
     }
 }
