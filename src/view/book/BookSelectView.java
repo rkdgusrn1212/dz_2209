4d7bf7fc138c18a0550b113ced0a29e62fa97d55
3002291 <112863906+3002291@users.noreply.github.com>
2022-09-08 11:51:03 +0900

    BookSelectView 퀴즈, 검색, 전체목록 조회 버튼 추가

--- src/view/book/BookSelectView.java
+++ src/view/book/BookSelectView.java
@@ -3,39 +3,45 @@ package view.book;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JTextField;
 
 import view.View;
 
 public class BookSelectView extends View {
     public JLabel labelMsg, labelId;
-    public JButton btnSelect, btnGenre, btnBack;
+    public JButton btnAllList, btnQuiz, btnSearch, btnMyPage, btnLogout;
     public BookClickView[] viewBookClick;
-    public JButton btnMyPage, btnLogout;
+    public JTextField tfSearch;
+    
     public BookSelectView() {
         setTitle("BookSelectView");
         setLayout(null);
         //new   
         labelMsg = new JLabel("도서를 선택하세요");
         labelId = new JLabel("id");
-        btnSelect = new JButton("도서 선택");
-        btnGenre = new JButton("장르 선택");
-//        btnBack = new JButton("로그 아웃");
-        
+        tfSearch = new JTextField();
+        btnAllList = new JButton("전체 목록");
+        btnSearch = new JButton("도서 검색");
+        btnQuiz = new JButton("도서 퀴즈");
         viewBookClick = new BookClickView[3];
         btnMyPage = new JButton("마이페이지");
         btnLogout = new JButton("로그아웃");
         //setBounds
         labelId.setBounds(100, 20, 80, 30);
-        labelMsg.setBounds(500, 20, 120, 30);
-        btnSelect.setBounds(250, 700, 100, 30);
-        btnGenre.setBounds(580, 700, 100, 30);
+        labelMsg.setBounds(440, 20, 120, 30);
+        tfSearch.setBounds(495, 700, 180, 30);
+        btnSearch.setBounds(680, 700, 100, 30);
+        btnAllList.setBounds(180, 700, 100, 30);
+        btnQuiz.setBounds(340, 700, 100, 30);
         btnMyPage.setBounds(680, 20, 100, 30);
         btnLogout.setBounds(820, 20, 100, 30);
         //add
         add(labelId);
         add(labelMsg);
-        add(btnSelect);
-        add(btnGenre);
+        add(tfSearch);
+        add(btnSearch);
+        add(btnAllList);
+        add(btnQuiz);
         add(btnMyPage);
         add(btnLogout);
         //add BookClickView
@@ -46,5 +52,6 @@ public class BookSelectView extends View {
         }
 
         setSize(1000,800);
+        setVisible(true);
     }
 }
