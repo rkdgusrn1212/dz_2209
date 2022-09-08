63921e8dc36b547a96ab2bd18a5c32e7eb4fb50d
3002291 <112863906+3002291@users.noreply.github.com>
2022-09-08 21:23:38 +0900

    Update FindIdPwdView.java

--- src/view/member/FindIdPwdView.java
+++ src/view/member/FindIdPwdView.java
@@ -1,5 +1,7 @@
 package view.member;
 
+import java.awt.Font;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -12,24 +14,35 @@ public class FindIdPwdView extends View{
 
     public JTextField tfEmail;
     JLabel labelEmail; //"로그인시 입력한 이메일을 입력하세요"
-    public JButton btnSubmit;
+    public JButton btnSubmit, btnReset;
 
     public FindIdPwdView() {
 
-        setTitle("아이디 비번 찾기");
+        setTitle("ID/비밀번호 찾기");
 
         tfEmail = new JTextField();
-        tfEmail.setBounds(50, 80, 180, 30);
-        labelEmail = new JLabel("로그인시 입력한 이메일을 입력하세요");
-        labelEmail.setBounds(30, 30, 250, 20);
+        tfEmail.setBounds(40, 80, 365, 35);
+        labelEmail = new JLabel("가입 시 입력한 이메일을 입력하세요");
+        labelEmail.setBounds(40, 30, 350, 35);
         btnSubmit = new JButton("입력");
-        btnSubmit.setBounds(110, 130, 70, 50);
+        btnSubmit.setBounds(40, 130, 130, 35);
+        btnReset = new JButton("취소");
+        btnReset.setBounds(275, 130, 130, 35);
+        // 폰트 생성
+        Font font=new Font("맑은고딕", Font.BOLD, 16);
+        tfEmail.setFont(font);
+        labelEmail.setFont(font);
+        btnSubmit.setFont(font);
+        btnReset.setFont(font);
+        
 
         setLayout(null);
         add(tfEmail);
         add(labelEmail);
         add(btnSubmit);
+        add(btnReset);
 
-        setBounds(800,300,300,300);
+        setBounds(730,300,450,240);
     }
 }
+
