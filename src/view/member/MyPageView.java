a8f7518d2713ae600549acdb45cb2bba32a0b7c0
3002291 <112863906+3002291@users.noreply.github.com>
2022-09-12 16:20:21 +0900

    도서수정(Edit) 버튼 추가. Point 관련 label 삭제. Quiz 관련 버튼 삭제

--- src/view/member/MyPageView.java
+++ src/view/member/MyPageView.java
@@ -9,48 +9,44 @@ import javax.swing.JLabel;
 import view.View;
 
 public class MyPageView extends View {
-    public JLabel labelMsg, labelCash, labelPoint;
-    public JButton btnUpdate, btnHistory, btnCash, btnLogout, btnBack;
+    public JLabel labelMsg, labelCash;
+    public JButton btnUpdate, btnEditBook, btnCash, btnLogout, btnBack;
     
     public MyPageView() {
         setTitle("MyPage");
         setLayout(null);
-        labelMsg = new JLabel();
-        labelCash = new JLabel("잔여 캐시: ");
-        labelPoint = new JLabel("잔여 포인트: ");
+        labelMsg = new JLabel(); //ID 띄우는 label
+        labelCash = new JLabel("보유 캐시: ");
         btnUpdate = new JButton("비밀번호 수정");
-        btnHistory = new JButton("이용 내역");
-        btnCash = new JButton("캐쉬 충전");
+        btnEditBook = new JButton("도서 정보 수정");
+        btnCash = new JButton("캐시 충전");
         btnLogout = new JButton("로그아웃");
         btnBack = new JButton("메인페이지");
         //setBounds
         btnBack.setBounds(240,280,150,35);
         labelMsg.setBounds(50, 30, 300, 35);
         labelCash.setBounds(50, 100, 150, 35);
-        labelPoint.setBounds(50, 150, 150, 35);
 
         btnUpdate.setBounds(50, 230, 150, 35);
-        btnHistory.setBounds(240, 230, 150, 35);
-        btnCash.setBounds(50, 280, 150, 35);
+        btnEditBook.setBounds(50, 280, 150, 35);
+        btnCash.setBounds(240, 230, 150, 35);
         btnLogout.setBounds(240, 340, 150, 35);
         
         Font font=new Font("맑은고딕", Font.BOLD, 16);
         labelMsg.setFont(font);
         labelCash.setFont(font);
-        labelPoint.setFont(font);
         btnUpdate.setFont(font);
-        btnHistory.setFont(font);
+        btnEditBook.setFont(font);
         btnCash.setFont(font);
         btnLogout.setFont(font);
         btnBack.setFont(font);
 
         add(labelMsg);
         add(labelCash);
-        add(labelPoint);
 
         add(btnUpdate);
         add(btnCash);
-        add(btnHistory);
+        add(btnEditBook);
         add(btnLogout);
         add(btnBack);
 
